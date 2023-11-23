108454eecf10ef5253383112e6bbd4268d6fe5d9
pohsharon <pohsharon8855@gmail.com>
2023-11-23 14:48:26 +0800

    update 23/11

---
+++ app/src/main/java/com/example/madassignment/quiz/Create_Course.java
@@ -0,0 +1,16 @@
+package com.example.madassignment.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.madassignment.R;
+
+public class Create_Course extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_course);
+    }
+}
---
+++ app/src/main/java/com/example/madassignment/quiz/Create_Quiz.java
@@ -0,0 +1,94 @@
+package com.example.madassignment.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.madassignment.R;
+
+public class Create_Quiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quiz);
+
+        // Add Question Logic
+        ImageButton addQues = findViewById(R.id.addQuesButton);
+        addQues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addQuestionLayout();
+            }
+        });
+
+        // RadioGroup Selection and Deselection Logic
+        RadioGroup options = findViewById(R.id.correctAns_radioGroup);
+        options.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                // Handle the checked state change of RadioButtons in the RadioGroup
+                if (checkedId == R.id.radioButton1){
+
+                }
+                else if (checkedId == R.id.radioButton2){
+
+                }
+                else if (checkedId == R.id.radioButton3){
+
+                }
+                else {
+
+                }
+            }
+        });
+
+        // Handle deselection by clicking on a selected RadioButton again
+        options.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                if (checkedId == -1) {
+                    // No RadioButton is checked, do something here if needed
+                } else {
+                    RadioButton checkedRadioButton = findViewById(checkedId);
+                    checkedRadioButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            options.clearCheck(); // Clear the checked RadioButton
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void addQuestionLayout(){
+        LayoutInflater inflater = getLayoutInflater();
+        View questionLayout = inflater.inflate(R.layout.layout_question, null);
+
+        EditText questionEditText = questionLayout.findViewById(R.id.QuestionEditText);
+        EditText option1EditText = questionLayout.findViewById(R.id.option1EditText);
+        EditText option2EditText = questionLayout.findViewById(R.id.option2EditText);
+        EditText option3EditText = questionLayout.findViewById(R.id.option3EditText);
+        EditText option4EditText = questionLayout.findViewById(R.id.option4EditText);
+
+        RadioGroup optionsRadioGroup = questionLayout.findViewById(R.id.correctAns_layout_radioButton);
+        RadioButton radioButton1 = questionLayout.findViewById(R.id.radioButton1);
+        RadioButton radioButton2 = questionLayout.findViewById(R.id.radioButton2);
+        RadioButton radioButton3 = questionLayout.findViewById(R.id.radioButton3);
+        RadioButton radioButton4 = questionLayout.findViewById(R.id.radioButton4);
+
+        // Set unique IDs or other properties if needed for these elements
+
+        LinearLayout questionContainer = findViewById(R.id.questionContainer);
+        questionContainer.addView(questionLayout);
+    }
+
+}
