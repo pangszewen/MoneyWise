7ff61d8e4b30be372a81cf3cccec79d1e1d16977
pohsharon <pohsharon8855@gmail.com>
2023-11-30 13:41:53 +0800

    database create course

---
+++ app/src/main/java/com/example/madassignment/quiz/Course.java
@@ -0,0 +1,62 @@
+package com.example.madassignment.quiz;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Course {
+    private String courseID;
+    private String courseTitle;
+    private String advisorID;
+    private LocalDateTime dateCreated;
+    private String description;
+
+    public Course(){
+    }
+
+    public Course(String courseID, String advisorID, String courseTitle){
+        this.courseID = courseID;
+        this.advisorID = advisorID;
+        this.courseTitle = courseTitle;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public String getAdvisorID() {
+        return advisorID;
+    }
+
+    public void setAdvisorID(String advisorID) {
+        this.advisorID = advisorID;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.dateCreated = LocalDateTime.parse(dateCreated, formatter);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- app/src/main/java/com/example/madassignment/quiz/Create_Course.java
+++ app/src/main/java/com/example/madassignment/quiz/Create_Course.java
@@ -1,22 +1,50 @@
 package com.example.madassignment.quiz;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.madassignment.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class Create_Course extends AppCompatActivity {
+    FirebaseFirestore db;
+    CollectionReference collectionReference;
+    Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course);
+//        Fragment_Course_Description fragment = (Fragment_Course_Description) getSupportFragmentManager().findFragmentById(R.id.fragment_course);
+//        View view = fragment.getView();
+        db = FirebaseFirestore.getInstance();
+
+        EditText titleInput = findViewById(R.id.course_title_input);
+//        EditText desc = view.findViewById(R.id.description_input);
+        Button saveButton = findViewById(R.id.save_course_button);
 
         Button descButton=findViewById(R.id.descButton);
         Button lessonButton=findViewById(R.id.lessonButton);
@@ -38,6 +66,42 @@ public class Create_Course extends AppCompatActivity {
                 replaceFragment(new Fragment_Course_Lessons());
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String courseTitle = titleInput.getText().toString();
+                createCourse(courseTitle);
+            }
+        });
+    }
+
+    private void createCourse(String courseTitle) {
+        Log.d("TAG", "createTitle");
+        CollectionReference collectionReference = db.collection("COURSE");
+        collectionReference.orderBy("dateCreated", Query.Direction.DESCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                ArrayList<Course> courseList = new ArrayList<>();
+                for (QueryDocumentSnapshot dc : task.getResult()) {
+                    Course course = convertDocumentToCourse(dc);
+                    courseList.add(course);
+                }
+                String courseID = generateCourseID(courseList);
+                String advisorID = "A0000001";
+                Course newCourse = new Course(courseID, advisorID, courseTitle);
+                insertTopicIntoDatabase(newCourse);
+            }
+        });
+    }
+
+    public Course convertDocumentToCourse(QueryDocumentSnapshot dc){
+        Course course = new Course();
+        course.setCourseID(dc.getId());
+        course.setAdvisorID(dc.get("userID").toString());
+        course.setDateCreated(dc.get("datePosted").toString());
+        course.setCourseTitle(dc.get("subject").toString());
+        return course;
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -45,5 +109,47 @@ public class Create_Course extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
+    }
+
+    private void insertTopicIntoDatabase(Course course) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("advisorID", course.getAdvisorID());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        String formattedDateTime = course.getDateCreated().format(formatter);
+//        map.put("dateCreated", formattedDateTime);
+        map.put("title", course.getCourseTitle());
+        db.collection("COURSE").document(course.getCourseID()).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(Create_Course.this, "Topic Successfully Posted", Toast.LENGTH_SHORT).show();
+                    Log.d("TAG", "uploaded");
+                }else {
+                    Toast.makeText(Create_Course.this, "Topic Failed to Post", Toast.LENGTH_SHORT).show();
+                    Log.d("TAG", "Failed");
+                }
+            }
+        });
+    }
+
+    private String generateCourseID(List<Course> courses){
+        String newID = null;
+        while(true) {
+            int randomNum = rand.nextInt(1000000);
+            newID = "C" + String.format("%07d", randomNum); //C0001000
+            if(checkDuplicatedTopicID(newID, courses))
+                break;
+        }
+        Log.d("TAG", "This is new courseID " + newID);
+        return newID;
+    }
+
+    private boolean checkDuplicatedTopicID(String newID, List<Course> courses){
+        for(Course topic: courses){
+            if(newID.equals(topic.getCourseID()))
+                return false;
+        }
+        Log.d("TAG", "This is checked topic ID " + newID);
+        return true;
     }
 }
