ca925f97eb9577a7615d959bdd204f9f68ec00bc
WChengJingWen <jingwen0408@gmail.com>
2023-12-07 23:51:20 +0800

    added color and solve bud for nav bar

--- app/src/main/java/com/example/madassignment/Expenses/MainActivity.java
+++ app/src/main/java/com/example/madassignment/Expenses/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.madassignment.Expenses;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -37,6 +39,37 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+
+        // Disable default icon tinting
+        binding.bottomNavigationViewExpenses.setItemIconTintList(null);
+        binding.bottomNavigationViewExpenses.setItemTextColor(null);
+
+        // Set custom icon tinting
+        int[][] states = new int[][]{
+                new int[]{android.R.attr.state_checked}, // checked
+                new int[]{-android.R.attr.state_checked}  // unchecked
+        };
+
+        int selectedIconColor = ContextCompat.getColor(this, R.color.dark_blue);
+        int unselectedIconColor = ContextCompat.getColor(this, R.color.white);
+        int selectedTextColor = ContextCompat.getColor(this, R.color.dark_blue);
+        int unselectedTextColor = ContextCompat.getColor(this, R.color.white);
+
+        int[] colors = new int[]{
+                selectedIconColor,
+                unselectedIconColor
+        };
+
+        int[] textColors = new int[]{
+                selectedTextColor,
+                unselectedTextColor
+        };
+
+        ColorStateList iconColorStateList = new ColorStateList(states, colors);
+        ColorStateList textColorStateList = new ColorStateList(states, textColors);
+
+        binding.bottomNavigationViewExpenses.setItemIconTintList(iconColorStateList);
+        binding.bottomNavigationViewExpenses.setItemTextColor(textColorStateList);
 
     }
 
