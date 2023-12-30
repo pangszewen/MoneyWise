baf2887b6fe6005376cca5720d51537e6732bc41
pohsharon <pohsharon8855@gmail.com>
2023-12-31 01:49:31 +0800

    done main page ui

---
+++ app/src/main/java/com/example/madassignment/quiz/main_page.java
@@ -0,0 +1,16 @@
+package com.example.madassignment.quiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.madassignment.R;
+
+public class main_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
